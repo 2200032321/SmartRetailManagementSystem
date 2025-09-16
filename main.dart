7aa8f7547bbbb54c34a92fd281a408972ee7855d
Preetham Sambi Reddy <146952458+2200032321@users.noreply.github.com>
2025-09-16 18:34:18 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,24 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:srms_v4/screens/dashboard.dart';
 import 'package:srms_v4/screens/login_screen.dart';
-
+import 'package:srms_v4/screens/splash_screen.dart'; // Import the splash screen
 
 void main() {
-  runApp(SRMSApp());
+  runApp(const MyApp());
 }
 
-class SRMSApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Smart Retail Management System with AI',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.indigo, // Updated to indigo for consistency
       ),
       initialRoute: '/',
       routes: {
-        '/': (context) => LoginPage(),
+        '/': (context) => LoginScreen(), // Login first
+        '/splash': (context) {
+          final token = ModalRoute.of(context)!.settings.arguments as String;
+          return SplashScreen(token: token);
+        },
         '/dashboard': (context) {
           final token = ModalRoute.of(context)!.settings.arguments as String;
           return Dashboard(token: token);
